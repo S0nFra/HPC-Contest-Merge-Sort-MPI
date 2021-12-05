77b1f12a9aaac691e5d5456121a6cfd8756e2ff7
SharpBro <mario07999@outlook.it>
2021-12-05 15:26:16 +0100

    Update inputDataGenerator.py

--- scripts/inputDataGenerator.py
+++ scripts/inputDataGenerator.py
@@ -1,10 +1,14 @@
 import numpy as np
 
+sizes = [16,18,19] # exponent of the size
+min = np.iinfo(np.int32).min
+max = np.iinfo(np.int32).max
 
 if __name__ == "__main__":
-    min = np.iinfo(np.int32).min
-    max = np.iinfo(np.int32).max
-    size = 2**24
-    filename = "2_24"
-    s = np.random.randint(min,max,size)
-    s.astype('int32').tofile(filename) 
+    
+    for size in sizes:
+        filename = f"2_{size}"
+        # create a vector of 2^(size) elements 
+        # with random integers in the range [INT_MIN,INT_MAX]
+        s = np.random.randint(min,max,2**size) 
+        s.astype('int32').tofile(filename) 
