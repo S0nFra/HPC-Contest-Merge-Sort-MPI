bb3f356d85a944bb5f2982d2fb93dd7c830af1a9
SharpBro <mario07999@outlook.it>
2021-12-06 12:36:03 +0100

    Update generate_measures.py

--- scripts/generate_measures.py
+++ scripts/generate_measures.py
@@ -3,11 +3,11 @@ from pathlib import Path
 import subprocess as sp
 from tqdm import tqdm
 from natural_sorting import *
+from math import log2
 
 SRC_FOLDER = Path.cwd().parent / Path('build/executables')
 DST_FOLDER = Path.cwd().parent / Path('measures')
 INPUT_FILES_PATH = Path.cwd().parent / Path('data')
-#VERSION_ONE_PATH = DST_FOLDER / Path("Version 1")
 
 INPUT_SIZE_LIST = (2**16,2**18,2**19)
 PROCS = (0, 1, 2, 4, 8, 16)
@@ -47,23 +47,34 @@ def generate_measures():
     mpi_executables = executables[:CASES] # first half CASES are mpi execs
     serial_executables = executables[CASES:] # second half CASES are serial execs
     
-    for case in range(CASES):
-        if(case == 1):
+    for case in range(CASES): # for each case
+        if(case == 0):
             CASE_PATH = CASE_ONE_PATH
         else:
             CASE_PATH = CASE_TWO_PATH
+            
+        exe_serial_path = Path(serial_executables[case]).resolve()
+        exe_mpi_path = Path(mpi_executables[case]).resolve()
         
         for version in VERSIONS: # for each version
             VERSION_PATH = CASE_PATH / Path(f"version_{version}")
             create_dir_if_not_exists(VERSION_PATH)
-            for size_in in INPUT_SIZE_LIST: # for each size
-                for meas in range(MSRS): # MSRS measures
-                    for proc_num in PROCS: # for each number of Computing Elements
-                        if(proc_num == 0): # serial
-                            pass
-                        else: # parallel
-                            pass
-            
+            for in_size_n,input_size in enumerate(INPUT_SIZE_LIST): # for each size
+                
+                input_file = Path(inputs[in_size_n]).resolve()
+                
+                for proc_num in PROCS: # for each number of Computing Elements
+                    in_size = f"{int(log2(input_size))}"
+                    if(proc_num == 0): # serial
+                        output_measures_path = VERSION_PATH / Path(f"serial_{in_size}.csv")
+                        command = f".{exe_serial_path} \'{input_file}\' {input_size}"
+                    else: # parallel
+                        output_measures_path = VERSION_PATH / Path(f"mpi_{proc_num}_{in_size}.csv")
+                        command = f"mpirun -np {proc_num} .{exe_mpi_path} \'{input_file}\' {input_size} {version}"
+                    
+                    #print(f"executing version {version}, input size {input_size} and {proc_num} CE with command: {command}")
+                    #print(output_measures_path)
+                    
     
     
 
