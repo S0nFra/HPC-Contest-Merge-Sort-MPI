925ad24acbf24bf275bdf5b4025b0af06a403b33
SharpBro <mario07999@outlook.it>
2021-12-28 11:03:15 +0100

    inserted copyright in each file and enabled doxygen in CmakeLists.txt

--- include/mergesort_serial.h
+++ include/mergesort_serial.h
@@ -8,6 +8,32 @@
  * @copyright Copyright (c) 2021
  * 
  */
+/** 
+ * Course: High Performance Computing 2021/2022
+ *
+ * Lecturer: Francesco Moscato    fmoscato@unisa.it
+ *
+ * Group:
+ * Mario Pellegrino    0622701671  m.pellegrino42@studenti.unisa.it
+ * Francesco Sonnessa   0622701672   f.sonnessa@studenti.unisa.it
+ *
+ * Copyright (C) 2021 - All Rights Reserved 
+ *
+ * This file is part of Contest - MPI.
+ *
+ * Contest - MPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Contest - MPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Contest - MPI.  If not, see <http://www.gnu.org/licenses/>. 
+ */
 #ifndef F15EE0A8_AB33_42A0_AAAF_C6B4A6C041DC
 #define F15EE0A8_AB33_42A0_AAAF_C6B4A6C041DC
 
--- include/utils.h
+++ include/utils.h
@@ -6,8 +6,34 @@
  * @version 0.1
  * 
  * @copyright Copyright (c) 2021
- * 
  */
+/** 
+ * Course: High Performance Computing 2021/2022
+ *
+ * Lecturer: Francesco Moscato    fmoscato@unisa.it
+ *
+ * Group:
+ * Mario Pellegrino    0622701671  m.pellegrino42@studenti.unisa.it
+ * Francesco Sonnessa   0622701672   f.sonnessa@studenti.unisa.it
+ *
+ * Copyright (C) 2021 - All Rights Reserved 
+ *
+ * This file is part of Contest - MPI.
+ *
+ * Contest - MPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Contest - MPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Contest - MPI.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
 #ifndef FDF65B0C_B221_4A62_8ABF_4B3AA35CA7F1
 #define FDF65B0C_B221_4A62_8ABF_4B3AA35CA7F1
 
